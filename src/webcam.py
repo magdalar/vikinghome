d282035577b4ba6781a0e5ba921d75033404215b
Eric Andresen <eric@magdalar.net>
2018-07-28 14:32:15 +0200

    Handle webcam failures gracefully

--- src/webcam.py
+++ src/webcam.py
@@ -23,19 +23,22 @@ class WebCam(object):
         pyglet.clock.unschedule(self.update)
 
     def update(self, dt=None):
-        response = requests.get(CAM_URL)
-        if response.status_code != 200:
-            print('Failed to fetch image from', CAM_URL, 'code was:', response.status_code)
-        size = len(response.content)
-        if dt:
-            self.total_bytes += size
-            self.total_time += dt
-            #print('Avg KiB/s:', int(self.total_bytes / self.total_time / 1024))
-        image_bytes = io.BytesIO(response.content)
-        self.image = pyglet.image.load('cam.jpg', file=image_bytes)
-        image_utils.CenterImage(self.image)
-        #print('WebCam image: origin:', (self.x, self.y), 'size:',
-        #      (self.image.width, self.image.height))
+        try:
+          response = requests.get(CAM_URL)
+          if response.status_code != 200:
+              print('Failed to fetch image from', CAM_URL, 'code was:', response.status_code)
+          size = len(response.content)
+          if dt:
+              self.total_bytes += size
+              self.total_time += dt
+              #print('Avg KiB/s:', int(self.total_bytes / self.total_time / 1024))
+          image_bytes = io.BytesIO(response.content)
+          self.image = pyglet.image.load('cam.jpg', file=image_bytes)
+          image_utils.CenterImage(self.image)
+          #print('WebCam image: origin:', (self.x, self.y), 'size:',
+          #      (self.image.width, self.image.height))
+        except e:
+            print('Failed to fetch webcam', e)
 
     def draw(self):
         self.image.blit(self.x, self.y)
