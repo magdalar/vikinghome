f0d836b1713fe6ded77340bb08067563d47b0906
Eric Andresen <eric@magdalar.net>
2018-07-29 18:46:39 +0200

    More logging and resilience to failures of the webcam.

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO
+
+- Adopt a logging framework
+  - Timestamps on log entries
+  - Rotating logs
+- Add Sonos controls
+- Add Philips Hue controls
--- src/webcam.py
+++ src/webcam.py
@@ -6,12 +6,14 @@ import image_utils
 
 
 CAM_URL = 'http://192.168.86.34/cam_pic.php'
+LOGGING_FREQUENCY_SECS = 300
 
 class WebCam(object):
     def __init__(self):
         super(WebCam, self).__init__()
         self.total_bytes = 0
         self.total_time = 0
+        self.last_errors = 0
         self.image = pyglet.resource.image('no_image_available.png')
         image_utils.CenterImage(self.image)
         self.schedule()
@@ -22,23 +24,31 @@ class WebCam(object):
     def unschedule(self):
         pyglet.clock.unschedule(self.update)
 
-    def update(self, dt=None):
+    def update(self, dt):
+        self.total_time += dt
         try:
           response = requests.get(CAM_URL)
           if response.status_code != 200:
-              print('Failed to fetch image from', CAM_URL, 'code was:', response.status_code)
-          size = len(response.content)
-          if dt:
-              self.total_bytes += size
-              self.total_time += dt
-              #print('Avg KiB/s:', int(self.total_bytes / self.total_time / 1024))
+              if not self.last_errors or self.total_time > LOGGING_FREQUENCY_SECS:
+                print('Failed to fetch image from', CAM_URL, 'code was:', response.status_code)
+              self.last_errors += 1
+              return
+          elif self.last_errors > 0:
+              print('Success! WebCam has restarted after', self.last_errors, 'errors.')
+          self.last_errors = 0
+          self.total_bytes += len(response.content)
           image_bytes = io.BytesIO(response.content)
           self.image = pyglet.image.load('cam.jpg', file=image_bytes)
           image_utils.CenterImage(self.image)
-          #print('WebCam image: origin:', (self.x, self.y), 'size:',
-          #      (self.image.width, self.image.height))
         except Exception as e:
-            print('Failed to fetch webcam', e)
+            if not self.last_errors or self.total_time > LOGGING_FREQUENCY_SECS:
+                print('Failed to fetch WebCam:', e)
+            self.last_errors += 1
+        else:
+            if self.total_time > LOGGING_FREQUENCY_SECS:
+                print('WebCam Average KiB/s:', int(self.total_bytes / self.total_time / 1024))
+                self.total_bytes = 0
+                self.total_time = 0
 
     def draw(self):
         self.image.blit(self.x, self.y)
